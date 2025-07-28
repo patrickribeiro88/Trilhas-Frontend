1865767f423fec134c38879b47ff5bce1416f2c6
patrick <patricktjf@gmail.com>
2025-07-28 19:51:15 -0300

    OT 04 Node JS Finalizada

--- Node JS/OT 04  - Node JS/ListaDeGames/index.js
+++ Node JS/OT 04  - Node JS/ListaDeGames/index.js
@@ -4,6 +4,7 @@ const express = require("express");
 //instancio o express e carregando a biblioteca do express dentro dessa const app
 const app = express();
 
+
 app.listen(3000, () => {
     console.log("Servidor rodando!");
 })
